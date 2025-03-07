9533ef2393f03ab95bd4240f9f53d43c167b3f10
HanquanL <Hanquanl0115@gmail.com>
2025-03-06 21:34:06 -0500

    add get_randomNumber()

--- sched.cpp
+++ sched.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 vector<string> outputTable;
+ifstream randomNumbers;
 
 class Process {
     public:
@@ -16,7 +17,15 @@ class Process {
         int ioBurst;
 };
 
+enum class ProcessStates{
+    CREATED,
+    READY,
+    RUNNING,
+    BLOCK,
+};
+
 Process get_processObj(string lineOfProcess);
+int get_randomNumber();
 
 int main(int argc, char *argv[]) {
     
@@ -25,10 +34,18 @@ int main(int argc, char *argv[]) {
     ifstream readFile (inputFile);
     string lineOfProcess;
     Process currentProcess;
+    string stringTotalRandomNumbers;
+    randomNumbers.open(rfile);
+    getline(randomNumbers, stringTotalRandomNumbers);
     while(getline(readFile, lineOfProcess)){
         currentProcess = get_processObj(lineOfProcess);
-        cout << currentProcess.arrivalTime << currentProcess.totalCpuTime << currentProcess.cpuBurst << currentProcess.ioBurst << endl;
+        // cout << currentProcess.arrivalTime << currentProcess.totalCpuTime << currentProcess.cpuBurst << currentProcess.ioBurst << endl; //for test purposes
+
     }
+    // cout << get_randomNumber() << endl; //for test purposes
+    // cout << get_randomNumber() << endl; //for test purposes
+    // cout << get_randomNumber() << endl; //for test purposes
+    // cout << get_randomNumber() << endl; //for test purposes
 
     
     return 0;
@@ -48,3 +65,18 @@ Process get_processObj(string lineOfProcess) {
     return process;
 }
 
+int get_randomNumber(){
+    string line;
+    int randomNumber;
+    if (!getline(randomNumbers, line)) {
+        // If end of file is reached, reset to the beginning
+        randomNumbers.clear(); // Clear EOF flag
+        randomNumbers.seekg(0, ios::beg); // Move to the beginning of the file
+        // Skip the first line again
+        getline(randomNumbers, line);
+        getline(randomNumbers, line);
+    }
+    istringstream iss(line);
+    iss >> randomNumber;
+    return randomNumber;
+}
