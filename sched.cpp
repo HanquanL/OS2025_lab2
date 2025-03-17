e319ef9a76c8617b79a68f31e4c3d093bef11464
HanquanL <Hanquanl0115@gmail.com>
2025-03-17 12:10:46 -0400

    redo simulationLoop()

--- sched.cpp
+++ sched.cpp
@@ -14,7 +14,6 @@ ifstream randomNumbers;
 vector<int> randvals;
 int randomRange, randomOffset = 0;
 int globalProcessId = 0;
-bool CALL_SCHEDULER = false;
 /*global accounting counters*/
 int totalCpuBusyTime = 0;
 int totalIoBusyTime = 0;
@@ -45,8 +44,7 @@ class Process {
         int state_ts;    // the time when the process changed its state
         int cpuWaitingTime;    // the time the process has been waiting in the run queue
         int ioTime;    // the time spent in I/O blocked
-        ProcessStates newState;
-        ProcessStates oldState;
+        ProcessStates State;
         int priority;
         int cuurentPriority;
 };
@@ -99,13 +97,6 @@ class EventQueue{
                 }
             }
             return false;
-        }
-
-        int get_next_event_time(){
-            if(events.empty()){
-                return -1;
-            }
-            return events.front()->timeStamp;
         }
 
         bool isEmpty(){
@@ -174,15 +165,12 @@ int main(int argc, char *argv[]) {
     while(getline(readFile, lineOfProcess)){
         Process* currentProcess = get_processObj(lineOfProcess);
         outcomeProcesses.push_back(currentProcess);
-        Event* newEvent = new Event();
-        newEvent->timeStamp = currentProcess->arrivalTime;
-        newEvent->process = currentProcess;
-        newEvent->transition = Transition::TRANS_TO_READY;
+        Event* newEvent = new Event(currentProcess->arrivalTime, currentProcess, Transition::TRANS_TO_READY);
         eventQueue.insertEvent(newEvent);
         // cout << currentProcess.arrivalTime << currentProcess.totalCpuTime << currentProcess.cpuBurst << currentProcess.ioBurst << endl; //for test purposes
     }
-    simulationLoop();
-    printOutcome();
+    //simulationLoop();
+    //printOutcome();
     // cout << mydrndom(10) << endl; //for test purposes
     // cout << mydrndom(10) << endl; //for test purposes
     // cout << mydrndom(10) << endl; //for test purposes
@@ -194,13 +182,13 @@ int main(int argc, char *argv[]) {
     // cout << mydrndom(32) << endl; //for test purposes
     // cout << mydrndom(12) << endl; //for test purposes
     // cout << "Processing events: " << endl;
-    // while(!eventQueue.isEmpty()){
-    //     Event* event = eventQueue.getEvent();
-    //     cout << "Time: " << event -> timeStamp
-    //         << " Process ID: " << event -> process -> processId
-    //         << " State: " << (int)event -> oldState << endl;
-    //     delete event;
-    // }
+    while(!eventQueue.isEmpty()){
+        Event* event = eventQueue.getEvent();
+        cout << "Time: " << event -> timeStamp
+            << " Process ID: " << event -> process -> processId
+            << " State: " << (int)event -> transition << endl;
+        delete event;
+    }
     
     return 0;
 }
@@ -221,7 +209,7 @@ Process* get_processObj(string lineOfProcess) {
     process->state_ts = arrivalTime;
     process->cpuWaitingTime = 0;
     process->ioTime = 0;
-    process-> newState = ProcessStates::CREATED;
+    process->State = ProcessStates::CREATED;
 
     return process;
 }
@@ -249,175 +237,7 @@ void simulationLoop(){
     Event* currentEvent;
     Process* CURRENT_RUNNING_PROCESS = nullptr;
     int CURRENT_TIME = 0;
-    while(currentEvent = eventQueue.getEvent()){
-        Process* currentProcess = currentEvent->process;    //the Process the event works on
-        CURRENT_RUNNING_PROCESS = currentProcess;
-        Transition currentTransition = currentEvent->transition;
-        CURRENT_TIME = currentEvent->timeStamp;
-        int timeInPrevState = CURRENT_TIME - currentProcess->state_ts;
-
-        switch(currentTransition){
-            case Transition::TRANS_TO_READY:{
-                // must come from BLOCKED or CREATED
-                // add to run queue, no event created
-                if(currentProcess->copy_totalCpuTime == 0){
-                    CALL_SCHEDULER = true;
-                    currentProcess->finishTime = CURRENT_TIME;
-                    scheduler.add_expired_process(currentProcess);
-                    break;
-                }
-                currentProcess->newState = ProcessStates::READY;
-                currentProcess->oldState = ProcessStates::CREATED;
-                currentProcess->state_ts = CURRENT_TIME;
-                scheduler.add_process(currentProcess);
-                CALL_SCHEDULER = true;
-                if(CURRENT_RUNNING_PROCESS != nullptr && CURRENT_RUNNING_PROCESS->processId == currentProcess->processId){
-                    CURRENT_RUNNING_PROCESS = nullptr;
-                }
-                break;
-            }
-            
-            case Transition::TRANS_TO_RUN: {
-                int cpu_burst_actual;
-                int randomCpuBurst = mydrndom(currentProcess->cpuBurst);
-                ProcessStates prevState = currentProcess->newState;
-                if(currentProcess->copy_cpuBurst ==0){
-                    cpu_burst_actual = randomCpuBurst;
-                    currentProcess->copy_cpuBurst = cpu_burst_actual;
-                }else{
-                    cpu_burst_actual = currentProcess->copy_cpuBurst;
-                }
-                if(cpu_burst_actual > currentProcess->totalCpuTime){
-                    cpu_burst_actual = currentProcess->totalCpuTime;
-                    currentProcess->copy_cpuBurst = cpu_burst_actual;
-                }
-                currentProcess->state_ts = CURRENT_TIME;
-                currentProcess->newState = ProcessStates::RUNNING;
-                currentProcess->oldState = ProcessStates::READY;
-                if(cpu_burst_actual <= scheduler.quantum){
-                    currentProcess->copy_totalCpuTime = currentProcess->copy_totalCpuTime - cpu_burst_actual;
-                    if(currentProcess->copy_totalCpuTime >0){
-                        // process not finished yet, go to block
-                        currentProcess->newState = ProcessStates::BLOCK;
-                        currentProcess->oldState = ProcessStates::RUNNING;
-                        Event* runEvent = new Event(CURRENT_TIME + cpu_burst_actual, currentProcess, Transition::TRANS_TO_BLOCK);
-                        eventQueue.insertEvent(runEvent);
-                        currentProcess->copy_cpuBurst = 0;
-                    }else{
-                        // process finished
-                        currentProcess->newState = ProcessStates::TERMINATED;
-                        currentProcess->oldState = ProcessStates::RUNNING;
-                        Event* runEvent = new Event(CURRENT_TIME + cpu_burst_actual, currentProcess, Transition::TRANS_TO_READY);
-                        eventQueue.insertEvent(runEvent);
-                    }
-                    CURRENT_TIME += cpu_burst_actual;
-                }else{
-                    currentProcess->copy_totalCpuTime = currentProcess->copy_totalCpuTime - min(currentProcess->copy_cpuBurst, scheduler.quantum);
-                    currentProcess->copy_cpuBurst = currentProcess->copy_cpuBurst - min(currentProcess->copy_cpuBurst,scheduler.quantum);
-                    if(currentProcess->copy_cpuBurst > 0){
-                        // process not finished yet, go to block
-                        currentProcess->newState = ProcessStates::READY;
-                        currentProcess->oldState = ProcessStates::RUNNING;
-                        Event* runEvent = new Event(CURRENT_TIME + scheduler.quantum, currentProcess, Transition::TRANS_TO_READY);
-                        eventQueue.insertEvent(runEvent);
-                    }else{
-                        if(currentProcess->copy_totalCpuTime > 0){
-                            // process not finished yet, go to block
-                            currentProcess->newState = ProcessStates::BLOCK;
-                            currentProcess->oldState = ProcessStates::RUNNING;
-                            Event* runEvent = new Event(CURRENT_TIME + min(currentProcess->copy_cpuBurst,scheduler.quantum), currentProcess, Transition::TRANS_TO_BLOCK);
-                            eventQueue.insertEvent(runEvent);
-                        }else{
-                            // process finished
-                            currentProcess->newState = ProcessStates::TERMINATED;
-                            currentProcess->oldState = ProcessStates::RUNNING;
-                            Event* runEvent = new Event(CURRENT_TIME + min(currentProcess->copy_cpuBurst,scheduler.quantum), currentProcess, Transition::TRANS_TO_READY);
-                            eventQueue.insertEvent(runEvent);
-                        }
-                        CURRENT_TIME += min(currentProcess->copy_cpuBurst, scheduler.quantum);
-                    }
-                }
-                // if(currentProcess->copy_totalCpuTime > 0){
-                //     int actualBurst = min(randomCpuBurst, currentProcess->copy_totalCpuTime);
-                //     currentProcess->finishTime += actualBurst;
-                //     currentProcess->copy_totalCpuTime -= actualBurst;
-                //     currentProcess->newState = ProcessStates::BLOCK;
-                //     currentProcess->oldState = ProcessStates::RUNNING;
-                //     currentProcess->state_ts = actualBurst;
-                //     Event* runEvent = new Event(CURRENT_TIME + actualBurst, currentProcess, Transition::TRANS_TO_BLOCK);
-                //     eventQueue.insertEvent(runEvent);
-                // }else{
-                //     currentProcess->newState = ProcessStates::TERMINATED;
-                //     currentProcess->oldState = ProcessStates::RUNNING;
-                //     Event* runEvent = new Event(CURRENT_TIME, currentProcess, Transition::TRANS_TO_TERMINATE);
-                //     eventQueue.insertEvent(runEvent);
-                //     CURRENT_RUNNING_PROCESS = nullptr;
-                // }
-                CURRENT_RUNNING_PROCESS = currentProcess;
-                break;
-            }
-            
-            case Transition::TRANS_TO_BLOCK: {
-                int randomIoBurst = mydrndom(currentProcess->ioBurst);
-                currentProcess->state_ts = CURRENT_TIME;
-                currentProcess->newState = ProcessStates::BLOCK;
-                currentProcess->oldState = ProcessStates::RUNNING;
-                currentProcess->ioTime += randomIoBurst;
-                // int randomIoBurst = mydrndom(currentProcess->ioBurst);
-                // currentProcess->ioTime += randomIoBurst;
-                // currentProcess->finishTime += randomIoBurst;
-                // currentProcess->copy_totalCpuTime -= randomIoBurst;
-                // currentProcess->state_ts = randomIoBurst;
-                // currentProcess->newState = ProcessStates::READY;
-                // currentProcess->oldState = ProcessStates::BLOCK;
-                // Event* runEvent = new Event(CURRENT_TIME + randomIoBurst, currentProcess, Transition::TRANS_TO_RUN);
-                // eventQueue.insertEvent(runEvent);
-                Event* runEvent = new Event(CURRENT_TIME + randomIoBurst, currentProcess, Transition::TRANS_TO_READY);
-                eventQueue.insertEvent(runEvent);
-                CALL_SCHEDULER = true;
-                if(CURRENT_RUNNING_PROCESS != nullptr && CURRENT_RUNNING_PROCESS->processId == currentProcess->processId){
-                    CURRENT_RUNNING_PROCESS = nullptr;
-                }
-                break;
-            }
-            
-            case Transition::TRANS_TO_PREEMPT: {
-                
-                CALL_SCHEDULER = true;
-                break;
-            }
-            case Transition::TRANS_TO_TERMINATE:{
-                
-                break;
-            }
-        }
-        delete currentEvent; // Free the memory of the event
-
-        // if(CALL_SCHEDULER){
-        //     CALL_SCHEDULER = false;
-        //     if(CURRENT_RUNNING_PROCESS == nullptr){
-        //         CURRENT_RUNNING_PROCESS = scheduler.get_next_process();
-        //     }else if(CURRENT_RUNNING_PROCESS != nullptr){
-        //         Event* runEvent = new Event(CURRENT_TIME, CURRENT_RUNNING_PROCESS, Transition::TRANS_TO_RUN);
-        //         eventQueue.insertEvent(runEvent);
-        //     }
-        // }
-        if(CALL_SCHEDULER){
-            if(eventQueue.getEvent()->timeStamp == CURRENT_TIME){
-                    continue;
-            }
-            CALL_SCHEDULER = false;    // reset the flag
-            if(CURRENT_RUNNING_PROCESS == nullptr){
-                currentProcess = scheduler.get_next_process();
-                if(currentProcess == nullptr){
-                    continue;
-                }
-                Event* runEvent = new Event(max(CURRENT_TIME, currentProcess->arrivalTime), currentProcess, Transition::TRANS_TO_RUN);
-                eventQueue.insertEvent(runEvent);
-            }
-                
-        }
-    }
+   
 }
 
 void printOutcome(){
